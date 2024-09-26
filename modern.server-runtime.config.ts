a093975818d7cb7cde3dc6b0af36ce415fce7986
yunhe <yunhe@bytedance.com>
2024-09-26 14:23:50 +0800

    fix: add server runtime config

---
+++ modern.server-runtime.config.ts
@@ -0,0 +1,8 @@
+import { appTools, defineConfig } from '@modern-js/app-tools';
+
+// https://modernjs.dev/en/configure/app/usage
+export default defineConfig({
+    server:{
+        baseUrl: '/spaces/Luminnn/Demohouse-Static'
+    },
+});
